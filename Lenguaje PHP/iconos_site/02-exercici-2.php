a81c647f9243d5db15d8939b3cb57c14126dc996
Shah Zaib <shebiraaja@gmail.com>
2023-09-26 19:36:16 +0200

    casi terminado home page

---
+++ Lenguaje PHP/iconos_site/02-exercici-2.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once ('./functions-structure.php');
+
+myHeader();
+myMenu();
+
+
+?>
+
---
+++ Lenguaje PHP/iconos_site/index.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once ('./functions-structure.php');
+
+myHeader();
+myMenu();
+
+?>
+
+
+<body>
+
+<body>
+
+<h1 class="text-center">Welcome Christmas Cards</h1>
+
+    
+<?php
+
+$numeroImagen = array();
+
+//definir array de imagenes
+for ($i=1; $i < 21; $i++) { 
+    array_push($numeroImagen, $i . ".png");
+}
+
+for ($i=0; $i < count($numeroImagen); $i++) { 
+   echo ("<div class='d-flex justify-content-center align-center'> <img src=./img/" . $numeroImagen[$i] . "> </div>");
+}
+
+// print_r($numeroImagen)
+
+
+?>
+
+
+</body>
+
+</body>
