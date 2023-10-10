f029b786b4827cde0f535bcc070a772aeccdd0ad
Shah Zaib <shebiraaja@gmail.com>
2023-10-10 10:40:11 +0200

    finished juego 2 dados

--- ejercicios/juegoDados/03-juego-2.php
+++ ejercicios/juegoDados/03-juego-2.php
@@ -1,11 +1,19 @@
 <?php
-    require_once './04-functions.php';
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    myHeader();
+session_start();
 
-    myMenu();
+require_once './04-functions.php';
+
+
+myHeader();
+
+myMenu();
+
+const MAX_TIRADAS_J2 = 5;
+
+
 
 ?>
 
@@ -17,6 +25,60 @@
         <h1 class="text-center mt-4">
             Juego 2
         </h1>
+
+        <div class="d-flex justify-content-around">
+
+
+            <?php
+
+            // inicioJugada();
+            $numTiradas = tirarDadoRecargaJ2();
+
+            echo "<p>Tirada numero  : " . $numTiradas . "</p>";
+
+
+            if($numTiradas == 0) {
+                echo "<h1> Pulsa F5 para jugar<h1>";
+            }else {
+                echo "<div>";
+                echo " <p>Jugador 1 </p>";
+                $numDadoRandomJ1 = tirarDado();
+                mostrarDado($numDadoRandomJ1);
+                echo "<p> $numDadoRandomJ1 </p>";
+                echo "</div>";
+    
+
+
+                echo "<div>";
+                echo "<p>Jugador 2 </p>";
+                $numDadoRandomJ2 = tirarDado();
+                mostrarDado($numDadoRandomJ2);
+                echo "<p> $numDadoRandomJ2 </p>";
+                echo "</div>";
+
+                calcularPuntos($numDadoRandomJ1, $numDadoRandomJ2);
+
+
+                echo "Puntos Jugador 1  : " . $_SESSION["puntosJ1"];
+                echo ("<br>");
+                echo "Puntos Jugador 2  : " . $_SESSION["puntosJ2"];
+
+
+                $mensaje = decidirGanador($numTiradas);
+
+                echo "<h2> $mensaje </h2>";
+
+
+            }
+
+
+            ?>
+        </div>
+
+
+
+    </div>
+
 
     </div>
 
--- ejercicios/juegoDados/04-juego-3.php
+++ ejercicios/juegoDados/04-juego-3.php
@@ -1,7 +1,8 @@
 <?php
+    declare(strict_types=1);
+    
     require_once './04-functions.php';
 
-    declare(strict_types=1);
 
 
     myHeader();
