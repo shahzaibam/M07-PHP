f1d55ee99a1db2cabec52fb8c4f402d309f68cfc
Shah Zaib <shebiraaja@gmail.com>
2023-10-11 13:52:33 +0200

    proyecto jugadores

---
+++ ejercicios/juegoJugadores/data.php
@@ -0,0 +1,88 @@
+<?php
+
+$jugadores = [
+    ["ansu"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "7",
+        ["fechaNacimiento"] => "31/10/2002",
+        ["rol"] => "Delantero",
+        ["goles"] => 1,
+        ["partidos"] => 8
+    ],
+    
+    ["balde"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "14",
+        ["fechaNacimiento"] => "18/10/2003",
+        ["rol"] => "Defensa",
+        ["goles"] => 0,
+        ["partidos"] => 6
+    ],
+
+    ["bryan"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "5",
+        ["fechaNacimiento"] => "09/09/2001",
+        ["rol"] => "Delantero",
+        ["goles"] => 0,
+        ["partidos"] => 2
+    ],
+
+    ["david"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "17",
+        ["fechaNacimiento"] => "14/02/1994",
+        ["rol"] => "Defensa",
+        ["goles"] => 0,
+        ["partidos"] => 1
+    ],
+
+    ["fabian"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "24",
+        ["fechaNacimiento"] => "03/04/1996",
+        ["rol"] => "Centrocampista",
+        ["goles"] => 1,
+        ["partidos"] => 19
+    ],
+
+    ["fran"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "27",
+        ["fechaNacimiento"] => "14/08/1999",
+        ["rol"] => "Defensa",
+        ["goles"] => 2,
+        ["partidos"] => 19
+    ],
+
+
+    ["gavi"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "9",
+        ["fechaNacimiento"] => "05/08/2004",
+        ["rol"] => "Centrocampista",
+        ["goles"] => 3,
+        ["partidos"] => 22
+    ],
+
+    ["jesus"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "18",
+        ["fechaNacimiento"] => "21/11/1985",
+        ["rol"] => "Defensa",
+        ["goles"] => 5,
+        ["partidos"] => 48
+    ],
+
+    ["joselu"] => [
+        ["pais"] => "España",
+        ["numCamiseta"] => "4",
+        ["fechaNacimiento"] => "27/03/1990",
+        ["rol"] => "Delantero",
+        ["goles"] => 4,
+        ["partidos"] => 6
+    ],
+
+]
+
+?>
