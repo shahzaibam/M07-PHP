b6624ecd2f436b40fb81ef9ad1d58354c7246403
Shah <shebiraaja@gmail.com>
2023-10-14 15:10:42 +0200

    ejercicio 3 started

---
+++ ejercicios/juegoJugadores/ejercicio-3.php
@@ -0,0 +1,28 @@
+<?php
+include('./layout.php');
+include('./functions.php');
+
+myHeader();
+myMenu();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1 class="text-center mt-4">Ejercicio 3</h1>
+    </div>
+
+    <?php
+
+    myFooter();
+
+
+    ?>
+</body>
+</html>
