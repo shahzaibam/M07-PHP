e8db93018841d271cbbdbc12d1d54bcd61ee3415
shebi1 <shebiraaja@gmail.com>
2023-10-25 13:57:17 +0200

    form send and get is done, validations are pending

--- Lenguaje PHP/HasbyPizza/pedidoForm.php
+++ Lenguaje PHP/HasbyPizza/pedidoForm.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 include("./layout-structure.php");
 
 myHeader();
@@ -17,7 +20,7 @@ myHeader();
                     <div class="card">
 
 
-                        <form class="form-card" action="ticket.php">
+                        <form class="form-card" action="ticket.php" method="post">
 
                             <div class="row justify-content-between text-left">
                                 <div class="form-group col-sm-6 flex-column d-flex"> <label for="name" class="form-control-label px-3">Name</label> <input type="text" id="name" name="name" placeholder="Enter your Name"> </div>
@@ -38,7 +41,7 @@ myHeader();
 
 
                             <div class="row justify-content-between mt-5">
-                                <div class="form-group col-sm-2 flex-column d-flex"> <label for="discount" class="form-control-label px-3">Discount</label> <input type="checkbox" id="discount" name="discount" placeholder=""> </div>
+                                <div class="form-group col-sm-2 flex-column d-flex"> <label for="discount" class="form-control-label px-3">Discount</label> <input type="checkbox" id="discount" name="discount" placeholder="" value="discount"> </div>
                             </div>
 
                             <div class="row justify-content-between text-left mt-5">
@@ -55,7 +58,7 @@ myHeader();
                                     <tbody>
                                         <tr>
                                             <th scope="row">
-                                                <label for="menu1" class="form-control-label px-3"></label> <input type="checkbox" id="menu1" name="menu1" placeholder="">
+                                                <label for="menu1" class="form-control-label px-3"></label> <input type="checkbox" id="menu1" name="menu1" placeholder="" value="Pepperoni + Drink">
                                             </th>
                                             <td>Pepperoni + Drink</td>
                                             <td>12€</td>
@@ -65,7 +68,7 @@ myHeader();
                                         </tr>
                                         <tr>
                                             <th scope="row">
-                                                <label for="menu2" class="form-control-label px-3"></label> <input type="checkbox" id="menu2" name="menu2" placeholder="">
+                                                <label for="menu2" class="form-control-label px-3"></label> <input type="checkbox" id="menu2" name="menu2" placeholder="" value="Barbeque + Water">
                                             </th>
                                             <td>Barbeque + Water</td>
                                             <td>10€</td>
@@ -75,7 +78,7 @@ myHeader();
                                         </tr>
                                         <tr>
                                             <th scope="row">
-                                                <label for="menu3" class="form-control-label px-3"></label> <input type="checkbox" id="menu3" name="menu3" placeholder="">
+                                                <label for="menu3" class="form-control-label px-3"></label> <input type="checkbox" id="menu3" name="menu3" placeholder="" value="Special HasbyPizza + Complements">
                                             </th>
                                             <td>Special HasbyPizza + Complements</td>
                                             <td>17€</td>
@@ -91,7 +94,7 @@ myHeader();
 
 
                             <div class="row justify-content-between mt-5">
-                                <div class="form-group col-sm-2 flex-column d-flex"> <label for="discount" class="form-control-label px-3">Custom Pizza</label> <input type="checkbox" id="discount" name="discount" placeholder=""> </div>
+                                <div class="form-group col-sm-2 flex-column d-flex"> <label for="discount" class="form-control-label px-3">Custom Pizza</label> <input type="checkbox" id="custom" name="custom" placeholder="" value="CustomPizza"> </div>
                             </div>
 
                             <div class="d-flex">
@@ -111,17 +114,43 @@ myHeader();
 
                                     <div class="form-group col-sm-2 flex-column">
                                         <label for="small" class="form-control-label px-3">Small</label>
-                                        <input type="radio" id="small" name="sizePizza" value="small">
+                                        <input type="radio" id="small" name="sizePizza[]" value="small">
                                     </div>
 
                                     <div class="form-group col-sm-2 flex-column">
                                         <label for="medium" class="form-control-label px-3">Medium</label>
-                                        <input type="radio" id="medium" name="sizePizza" value="medium">
+                                        <input type="radio" id="medium" name="sizePizza[]" value="medium">
                                     </div>
 
                                     <div class="form-group col-sm-2 flex-column">
                                         <label for="big" class="form-control-label px-3">Big</label>
-                                        <input type="radio" id="big" name="sizePizza" value="big">
+                                        <input type="radio" id="big" name="sizePizza[]" value="big">
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            <div class="d-flex">
+
+                                <div class="row text-left mt-5 ml-4">
+                                    <div class="form-group col-sm-3 flex-column">
+                                        <span>Tamaño:</span>
+                                    </div>
+
+                                    <div class="form-group col-sm-3 flex-column">
+                                        <label for="masa" class="form-control-label px-2">Masa Roll</label>
+                                        <input type="radio" id="masa" name="dough[]" value="Masa Roll">
+                                    </div>
+
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <label for="finizzima" class="form-control-label px-3">Finizzima</label>
+                                        <input type="radio" id="finizzima" name="dough[]" value="Finizzima">
+                                    </div>
+
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <label for="classic" class="form-control-label px-3">Classic</label>
+                                        <input type="radio" id="classic" name="dough[]" value="Classic">
                                     </div>
                                 </div>
 
@@ -129,11 +158,78 @@ myHeader();
 
 
 
+                            <div class="d-flex">
 
-                            <!-- 
+                                <div class="row text-left mt-5 ml-4">
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <span>Ingredientes:</span>
+                                    </div>
+
+                                    <div class="form-group col-sm-1 flex-column">
+                                        <label for="chicken" class="form-control-label px-2">Chicken</label>
+                                        <input type="checkbox" id="chicken" name="ingredients[]" value="chicken">
+                                    </div>
+
+                                    <div class="form-group col-sm-1 flex-column">
+                                        <label for="veal" class="form-control-label px-3">Veal</label>
+                                        <input type="checkbox" id="veal" name="ingredients[]" value="finizzima">
+                                    </div>
+
+                                    <div class="form-group col-sm-1 flex-column">
+                                        <label for="egg" class="form-control-label px-3">Egg</label>
+                                        <input type="checkbox" id="egg" name="ingredients[]" value="egg">
+                                    </div>
+
+                                    <div class="form-group col-sm-1 flex-column">
+                                        <label for="tuna" class="form-control-label px-3">Tuna</label>
+                                        <input type="checkbox" id="tuna" name="ingredients[]" value="tuna">
+                                    </div>
+
+
+                                    <div class="form-group col-sm-1 flex-column">
+                                        <label for="mushrooms" class="form-control-label px-3">Mushrooms</label>
+                                        <input type="checkbox" id="mushrooms" name="ingredients[]" value="mushrooms">
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            <div class="d-flex">
+
+                                <div class="row text-left mt-5 ml-4">
+                                    <div class="form-group col-sm-3 flex-column">
+                                        <span>Extras:</span>
+                                    </div>
+
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <label for="olive" class="form-control-label px-2">Olive (0,25)</label>
+                                        <input type="checkbox" id="olive" name="extras[]" value="olive">
+                                    </div>
+
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <label for="bacon" class="form-control-label px-3">Bacon (0,50)</label>
+                                        <input type="checkbox" id="bacon" name="extras[]" value="bacon">
+                                    </div>
+
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <label for="onion" class="form-control-label px-3">Onion (0,25)</label>
+                                        <input type="checkbox" id="onion" name="extras[]" value="onion">
+                                    </div>
+
+                                    <div class="form-group col-sm-2 flex-column">
+                                        <label for="potato" class="form-control-label px-3">Potato (0,50)</label>
+                                        <input type="checkbox" id="potato" name="extras[]" value="potato">
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+                            
                             <div class="row justify-content-end">
                                 <div class="form-group col-sm-6"> <input type="submit" class="btn-block btn-primary"> </div>
-                            </div> -->
+                            </div>
 
 
                         </form>
---
+++ Lenguaje PHP/HasbyPizza/ticket.php
@@ -0,0 +1,143 @@
+<?php
+
+session_start();
+
+include("./layout-structure.php");
+myHeader();
+
+
+if (isset($_POST['sizePizza'])) {
+    // $_SESSION['sizePizza'] = $_POST['sizePizza'][0]; // Assuming only one option can be selected
+
+    for ($i = 0; $i < count($_POST['sizePizza']); $i++) {
+        $sizePizza = $_POST['sizePizza'][$i];
+    }
+
+    // echo "Selected size: " . $selectedSize;
+} else {
+    echo "error en size pizza";
+}
+
+
+if (isset($_POST['dough'])) {
+    // $_SESSION['sizePizza'] = $_POST['sizePizza'][0]; // Assuming only one option can be selected
+
+    for ($i = 0; $i < count($_POST['dough']); $i++) {
+        $dough = $_POST['dough'][$i];
+    }
+
+    // echo "Selected size: " . $selectedSize;
+} else {
+    echo "error en size pizza";
+}
+
+
+if (isset($_POST['ingredients'])) {
+
+    // foreach ($_POST['ingredients'] as $key => $value) {
+    //     $_POST['ingredients'];
+    //     array_push($_POST["ingredients"], $value);
+
+
+    // }
+
+} else {
+    echo "error en size pizza";
+}
+
+
+if (isset($_POST['extras'])) {
+    // $_SESSION['sizePizza'] = $_POST['sizePizza'][0]; // Assuming only one option can be selected
+
+    // for ($i = 0; $i < count($_POST['extras']); $i++) {
+    //     $_SESSION['extras'] = $_POST['extras'][$i];
+    // }
+
+    // echo "Selected size: " . $selectedSize;
+} else {
+    echo "error en size pizza";
+}
+
+$_SESSION['name'] = $_POST['name'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$city = $_POST['city'];
+$discount = $_POST['discount'];
+$menu1 = $_POST['menu1'];
+$quantity1 = $_POST['quantity1'];
+$menu2 = $_POST['menu2'];
+$quantity2 = $_POST['quantity2'];
+$menu3 = $_POST['menu3'];
+$quantity3 = $_POST['quantity3'];
+$custom = $_POST['custom'];
+$quantityCustom = $_POST['quantityCustom'];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket</title>
+</head>
+
+<body>
+    <div class="d-flex text-center justify-content-center">
+        <div>
+            <div class="info border m-5">
+                <h1>Thanks for buying in HasbyPizza.</h1>
+                <p>Name: <?php echo $_SESSION["name"]; ?></p>
+                <p>Surname: <?php echo $surname; ?></p>
+                <p>Email: <?php echo $email ?></p>
+                <p>city: <?php echo $city; ?></p>
+                <p>Discount: <?php echo $discount; ?></p>
+                <p>Menu1: <?php echo $menu1; ?></p>
+                <p>Quantity1: <?php echo $quantity1; ?></p>
+                <p>Menu2: <?php echo $menu2; ?></p>
+                <p>Quantity2: <?php echo  $quantity2; ?></p>
+                <p>menu3: <?php echo $menu3; ?></p>
+                <p>quantity3: <?php echo $quantity3; ?></p>
+                <p>custom: <?php echo $custom; ?></p>
+                <p>QuantityCustom: <?php echo $quantityCustom; ?></p>
+                <p>SizePizza: <?php echo $sizePizza; ?></p>
+                <p>Dough: <?php echo $dough; ?></p>
+
+
+                <p>ingredients: <?php
+
+                                for ($i = 0; $i < count($_POST['ingredients']); $i++) {
+                                    echo $_POST["ingredients"][$i] . ", ";
+                                }
+
+                                ?>
+
+                </p>
+
+
+
+                <p>extras: <?php
+
+                            for ($i = 0; $i < count($_POST['extras']); $i++) {
+                                echo $_POST["extras"][$i] . ", ";
+                            }
+
+                            ?>
+                </p>
+
+            </div>
+
+
+        </div>
+
+
+        <div class="hasbyThankPicture info  m-5">
+            <img src="./img/hasbyThumbsUp.jpg" alt="" height="608.15px">
+        </div>
+
+</body>
+
+</html>
