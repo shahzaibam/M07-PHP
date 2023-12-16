18f9ac319dfc70b1770484c3fda7a0887fb5667b
Shah Zaib <shebiraaja@gmail.com>
2023-12-16 02:11:49 +0100

    Creado la clase Clubs y mostrado los clubs, logos dinamicamente con una clase

--- UF2/Examen UF1 hecho en POO/examen UF1.2023.24. LLiga ACB/inc/Clubs.php
+++ UF2/Examen UF1 hecho en POO/examen UF1.2023.24. LLiga ACB/inc/Clubs.php
@@ -5,6 +5,56 @@ class Clubs {
 
     private $image;
 
+    /**
+     * @param $nomClub
+     * @param $image
+     */
+    public function __construct($nomClub)
+    {
+        $this->nomClub = $nomClub;
+        $this->image = $this->nomClub . ".png";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomClub()
+    {
+        return $this->nomClub;
+    }
+
+    /**
+     * @param mixed $nomClub
+     */
+    public function setNomClub($nomClub): void
+    {
+        $this->nomClub = $nomClub;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function __toString()
+    {
+        $message = "";
+
+        $message .= "<div>";
+        $message .= "Nombre del Club --> " . $this->nomClub . "<br>";
+        $message .= " Ruta de la Imagen --> " . $this->image;
+        $message .= "<br>";
+        $message .= "</div>";
+
+
+        return $message;
+    }
+
 
 }
 
---
+++ UF2/Examen UF1 hecho en POO/examen UF1.2023.24. LLiga ACB/inc/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clubs</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+
+<?php
+
+include_once("./Clubs.php");
+
+/**
+ * en esta función leemos el fichero clubs y almacenamos todos los clubs en un array
+ * @return array
+ */
+function getAllClubs()
+{
+    $filename = '../clubs.txt';
+    $clubs = [];
+
+    if (file_exists($filename)) {
+        $contents = file($filename, FILE_IGNORE_NEW_LINES); // Leemos el archivo y almacenamos cada línea en un elemento del arreglo
+        if (!empty($contents)) {
+            foreach ($contents as $numeroFrase => $frase) {
+                $clubs[$numeroFrase] = $frase;
+            }
+        } else {
+            $clubs[] = "No hay Frases";
+        }
+    }
+
+    return $clubs;
+}
+
+$allClubs = getAllClubs();
+
+echo "<div class='club-container'>";
+
+/**
+ * si clubs no está vacío crearemos un array con diferentes claves que serian club_$numeroClub y instanciamos ahí, luego mostramos la instancia creada.
+ */
+if (!empty($allClubs)) {
+
+    foreach ($allClubs as $numeroClub => $club) {
+
+        $nombreVariable = "club_" . $numeroClub;
+
+        $instanciaClub[$nombreVariable] = new Clubs($club);
+
+        if ($instanciaClub[$nombreVariable] != $instanciaClub["club_0"]) {
+            echo "<div class='club-card'>";
+            echo $instanciaClub[$nombreVariable];
+
+            $image_src = $instanciaClub[$nombreVariable]->getImage();
+
+            // aquí muestro las imágenes, hago un get del nombre de la imagen y lo muestro
+            echo "<a href='./players_$club.php'><img class='club-logo' src='../images/logos_clubs/$image_src' alt='$club Logo'/></a>";
+
+            echo "</div>";
+        }
+    }
+} else {
+    echo "No hay nada";
+}
+
+echo "</div>";
+
+
+
+
+?>
+
+</body>
+</html>
