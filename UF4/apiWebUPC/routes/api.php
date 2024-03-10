2c5116c4564bafdcad4dcfd595e7e65ed04a55da
shahzaibam <shahzaibam05@gmail.com>
2024-03-10 23:16:03 +0100

    upc api done

--- UF4/apiWebUPC/routes/api.php
+++ UF4/apiWebUPC/routes/api.php
@@ -18,12 +18,15 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-
 Route::post('/register', [\App\Http\Controllers\Api\UserController::class, 'register'])->name('users.register');
 Route::post('/login', [\App\Http\Controllers\Api\UserController::class, 'login'])->name('users.login');
 Route::get('/eventsAll', [\App\Http\Controllers\Api\EventController::class, 'index'])->name('events.all');
 Route::get('/tournamentsAll', [\App\Http\Controllers\Api\TournamentController::class, 'index'])->name('tournaments.all');
 
+// Nueva ruta para obtener el tipo de usuario
+Route::middleware(['jwt.auth'])->get('/userType', function (Request $request) {
+    return response()->json(['userType' => $request->user()->type]); // Suponiendo que el tipo de usuario se almacena en el campo 'type'
+});
 
 // Rutas protegidas con JWT
 Route::middleware(['jwt.auth'])->group(function () {
@@ -33,13 +36,9 @@ Route::middleware(['jwt.auth'])->group(function () {
     Route::put('/events/{id}', [\App\Http\Controllers\Api\EventController::class, 'update'])->name('events.update');
     Route::delete('/events/{id}', [\App\Http\Controllers\Api\EventController::class, 'destroy'])->name('events.destroy');
 
-
-
     Route::get('/tournaments', [\App\Http\Controllers\Api\TournamentController::class, 'index'])->name('events.index');
     Route::get('/my-tournaments', [\App\Http\Controllers\Api\TournamentController::class, 'myTournaments'])->name('events.myTournaments');
     Route::post('/tournaments', [\App\Http\Controllers\Api\TournamentController::class, 'store'])->name('events.store');
     Route::put('/tournaments/{id}', [\App\Http\Controllers\Api\TournamentController::class, 'update'])->name('events.update');
     Route::delete('/tournaments/{id}', [\App\Http\Controllers\Api\TournamentController::class, 'destroy'])->name('events.destroy');
 });
-
-
